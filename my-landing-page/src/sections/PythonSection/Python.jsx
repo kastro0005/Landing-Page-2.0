f5d66c903be54eacd4a3eb604269ba17fa978b2b
Adrian Castro <70278376+kastro0005@users.noreply.github.com>
2024-10-07 22:42:08 -0400

    Python django imagen

--- my-landing-page/src/sections/PythonSection/Python.jsx
+++ my-landing-page/src/sections/PythonSection/Python.jsx
@@ -1,22 +1,37 @@
+import React from 'react';
 import Card from "./components/card";
 import { Items } from "./constant/items";
 
 export default function Python() {
   return (
-    <section className="py-16 bg-gray-200 ">
+    <section className="py-16 bg-gradient-to-r from-indigo-500 to-purple-600">
       <div className="container px-4 mx-auto">
-        <h2 className="mb-8 text-3xl font-bold">
-          Django & Python: Robust Server-side Solutions
-        </h2>
-        <div className="space-y-6">
-          {Items.map((item) => (
-            <Card
-              key={item.id}
-              id={item.id}
-              title={item.title}
-              content={item.content}
-            />
-          ))}{" "}
+        <div className="flex flex-col lg:flex-row items-center">
+          <div className="lg:w-1/2 lg:pr-8 mb-8 lg:mb-0">
+            <h2 className="mb-8 text-3xl font-bold text-white">
+              Django & Python: Robust Server-side Solutions
+            </h2>
+            <div className="space-y-6">
+              {Items.map((item) => (
+                <Card
+                  key={item.id}
+                  id={item.id}
+                  title={item.title}
+                  content={item.content}
+                />
+              ))}
+            </div>
+          </div>
+          <div className="lg:w-1/2">
+            <div className="relative">
+              <img
+                src="/../../../src/assets/img/Python.png"
+                alt="Server visualization"
+                className="rounded-lg shadow-xl"
+              />
+              <div className="absolute inset-0 bg-blue-500 opacity-20 rounded-lg"></div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
--- my-landing-page/src/sections/PythonSection/components/card.jsx
+++ my-landing-page/src/sections/PythonSection/components/card.jsx
@@ -1,13 +1,13 @@
 // eslint-disable-next-line react/prop-types
 const Card = ({ id, title, content }) => {
   return (
-    <div className="flex items-center">
-      <div className="flex items-center justify-center w-8 h-8 mr-4 text-white bg-blue-600 rounded-full">
+    <div className="flex items-start">
+      <div className="flex-shrink-0 flex items-center justify-center w-8 h-8 mr-4 text-white bg-blue-500 rounded-full">
         {id}
       </div>
       <div>
-        <h3 className="text-xl font-semibold">{title}</h3>
-        <p>{content}</p>
+        <h3 className="text-xl font-semibold text-white mb-2">{title}</h3>
+        <p className="text-gray-200">{content}</p>
       </div>
     </div>
   );
